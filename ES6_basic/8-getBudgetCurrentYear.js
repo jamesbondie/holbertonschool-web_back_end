bb8bce229bf4d656edb03fb7510123f87b542bf2
jamesbondie <jamesalfreddie@gmail.com>
2024-07-21 13:59:53 +0400

    current

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,13 +4,13 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
+
+
   const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita
+    [`capita-${getCurrentYear()}`]: capita,
   };
-
-
 
   return budget;
 }

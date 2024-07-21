12630d89db7914117e61b8e7013b5b029328b567
jamesbondie <jamesalfreddie@gmail.com>
2024-07-21 19:10:34 +0400

    typechecking

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -2,7 +2,13 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
+    if (typeof this._amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
     this._amount = amount;
+    if (!(this._currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of the Currency class');
+    }
     this._currency = currency;
   }
 
@@ -24,10 +30,16 @@ export default class Pricing {
   }
 
   set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
     this._amount = amount;
   }
 
   set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of the Currency class');
+    }
     this._currency = currency;
   }
 

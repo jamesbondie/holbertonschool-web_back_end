c8b6f2b61d0895c17962385ea0f5471ee94dfd23
jamesbondie <jamesalfreddie@gmail.com>
2024-07-26 11:10:05 +0400

    array corrected

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default function getListStudentIds(arr) {
-    if (students instanceof Array) {
+    if (arr instanceof Array) {
         return arr.map((x) => x.id)
     }
     return []

cf2f8c1928da9000cd61a6669e40711f05f4c167
jamesbondie <jamesalfreddie@gmail.com>
2024-07-26 11:21:13 +0400

    filtered

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 export default function updateStudentGradeByCity(students, city, newGrades) {
-    const lockies = students.filter((student) => student.location === city && student.id === newGrades.studentId);
-    
+    const lockies = students.filter((student) => student.location === city);    
     const idlockies = lockies.map((x) => {
         if (newGrades.grade) {
             x.grade = newGrades.grade

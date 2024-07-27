5897f84fd21f376001a4d5b07bcc12213751ea35
jamesbondie <jamesalfreddie@gmail.com>
2024-07-27 08:25:25 +0400

    declaration

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,16 +1,15 @@
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-    const promise1 = new Promise((resolve, reject) => {
-        promise1.then(() => {
+    return promise.then(() => {
             console.log("Got a response from the API")
             return {
                 status : 200,
                 body : 'success'
             }
         })
-        promise1.catch(() => {
+    .catch(() => {
             console.log("Got a response from the API")
             return new Error()
         })
-    })
-}
+    }
+

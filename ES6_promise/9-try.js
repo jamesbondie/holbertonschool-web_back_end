03d663c600ded89b9c3cb2d7e06a21eef3681027
jamesbondie <jamesalfreddie@gmail.com>
2024-07-27 09:08:30 +0400

    grammar issues

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -6,7 +6,7 @@ export default function guardrail(mathFunction) {
         queue.push(result)
     }
     catch(error) {
-        queue.push(`Error ${ error.message }`)
+        queue.push(`Erro: ${ error.message }`)
     }
     queue.push("Guardrail was processed")
     return queue

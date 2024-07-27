c263a257f09f675bacc3b4c35ca6e06ead699dd3
jamesbondie <jamesalfreddie@gmail.com>
2024-07-27 08:43:55 +0400

    promise.all

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,11 +3,7 @@ import { uploadPhoto } from "./utils";
 import { createUser } from "./utils";
 
 export default function handleProfileSignup() {
-    const p3 = Promise.resolve(uploadPhoto)
-    p3.then(() => {
-        console.log(`${uploadPhoto.body} ${createUser[1].firstName} ${createUser[1].lastName}`);
-    })
-    p3.catch(() => {
-        console.log("Signup system offline")
-    })
-}
+    return Promise.all([uploadPhoto(), createUser()])
+      .then((x) => console.log(`${x[0].body} ${x[1].firstName} ${x[1].lastName}`))
+      .catch(() => console.log('Signup system offline'));
+  }

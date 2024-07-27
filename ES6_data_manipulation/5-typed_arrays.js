86c428ad1386b5576a8de6d79f13f8b585acf39d
jamesbondie <jamesalfreddie@gmail.com>
2024-07-27 06:46:35 +0400

    dataview

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -5,9 +5,9 @@ export default function createInt8TypedArray(length, position, value) {
     }
     
     const buffer = new ArrayBuffer(length)
-    const int8 = new Int8Array(buffer)
+    const int8 = new DataView(buffer)
 
     int8[position] = value
     
-    return buffer
+    return int8
 }

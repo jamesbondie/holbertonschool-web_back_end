3984a181527edc93f366914277a4ca9a07460646
jamesbondie <jamesalfreddie@gmail.com>
2024-07-27 06:28:55 +0400

    finder

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,8 +2,9 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
     const lockies = students.filter((student) => student.location === city);    
     const idlockies = lockies.map((x) => {
-        if (newGrades.grade) {
-            x.grade = newGrades.grade
+        const xy = newGrades.find((y) => y.studentId === y.id)
+        if (xy) {
+            x.grade = xy.grade
         }
         else {
             x.grade = "N/A"

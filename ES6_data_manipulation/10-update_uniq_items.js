97f8ab1e5a5f7a9b8375b0a7fe49e94fe8a069ec
jamesbondie <jamesalfreddie@gmail.com>
2024-07-27 07:24:13 +0400

    error

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,8 +1,7 @@
 /* eslint-disable */
 export default function updateUniqueItems(map) {
-    if (!map instanceof Map) {
+    if (!(map instanceof Map))
         throw new Error("Cannot process")
-    }
     map.forEach((value, key) => {
         if (value == 1)
             map.set(key, 100)

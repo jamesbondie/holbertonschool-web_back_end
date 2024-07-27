1155a056cfd2a04844304718fc745ae3fd2eb349
jamesbondie <jamesalfreddie@gmail.com>
2024-07-27 09:24:51 +0400

    ternary

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,9 +5,10 @@ import uploadPhoto from "./5-photo-reject";
 export default function handleProfileSignup(firstName, lastName, fileName) {
     const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)]
     return Promise.allSettled(promises)
-    .then((promise) => [{
-        status : promise.status,
-        value : promise.value
-    },
-])
+    .then((promise) => 
+        promise.map(result => ({
+            status: result.status,
+            value: result.status === 'fulfilled' ? result.value : result.reason
+        }))
+    );
 }

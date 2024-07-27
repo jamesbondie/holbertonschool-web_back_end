fe34ee648c6bcdc18ee0e74e26f58fd9826ac029
jamesbondie <jamesalfreddie@gmail.com>
2024-07-27 06:47:43 +0400

    setint

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -7,7 +7,7 @@ export default function createInt8TypedArray(length, position, value) {
     const buffer = new ArrayBuffer(length)
     const int8 = new DataView(buffer)
 
-    int8[position] = value
+    int8.setInt8(position, value)
     
     return int8
 }

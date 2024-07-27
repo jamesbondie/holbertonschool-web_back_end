bea89712e849d366d5f11bc7378a7c30e97dea76
jamesbondie <jamesalfreddie@gmail.com>
2024-07-27 08:39:28 +0400

    one console

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -5,8 +5,7 @@ import { createUser } from "./utils";
 export default function handleProfileSignup() {
     const p3 = Promise.resolve(uploadPhoto)
     p3.then(() => {
-        console.log(uploadPhoto.body)
-        console.log(Object.values(createUser))
+        console.log(`${uploadPhoto.body} ${createUser[1].firstName} ${createUser[1].lastName}`);
     })
     p3.catch(() => {
         console.log("Signup system offline")

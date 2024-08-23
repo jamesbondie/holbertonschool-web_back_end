cd8396e03cd96503c3bdcd77769fe43588a7a7f0
jamesbondie <jamesalfreddie@gmail.com>
2024-08-23 22:06:51 +0400

    compherension corrected

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -10,6 +10,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Measures and returns the time taken to execute four comprehensions."""
     start_time: float = time.time()
-    await asyncio.gather(async_comprehension() for _ in range(4))
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
     end_time = time.time()
     return end_time - start_time

c891bb301a56cce78e705e33a8625f2437f94309
jamesbondie <jamesalfreddie@gmail.com>
2024-08-23 21:38:15 +0400

    annotation deleeted

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,14 +7,9 @@ import asyncio
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> float:
+async def measure_runtime():
     """Measures and returns the time taken to execute four comprehensions."""
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
+    await asyncio.gather(async_comprehension(), async_comprehension(), async_comprehension(), async_comprehension())
     end_time = time.time()
     return end_time - start_time

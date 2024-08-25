65696e52c0bb1633b663e376c6276c4b1a48803d
jamesbondie <jamesalfreddie@gmail.com>
2024-08-25 22:19:18 +0400

    pycode

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,5 +4,5 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Range of pages"""
+    """Range of pages based on our input and return tuple"""
     return ((page - 1) * page_size, page * page_size)

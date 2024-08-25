c691edc7c45a9000d2d1b7e4eb8078e721114ba7
jamesbondie <jamesalfreddie@gmail.com>
2024-08-25 22:13:54 +0400

    module

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,4 +4,4 @@
 
 def index_range(page: int, page_size: int) -> tuple:
     """Range of pages"""
-    return ((page - 1) * page_size, (page - 1) * page_size + page_size)
+    return (page - 1) * page_size, (page - 1) * page_size + page_size

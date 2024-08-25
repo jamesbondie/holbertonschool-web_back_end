5d2f42c7c355d5d71c8bcccef10108a63f801d04
jamesbondie <jamesalfreddie@gmail.com>
2024-08-25 22:16:32 +0400

    module

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Simple helper function module"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Range of pages"""
     return (page - 1) * page_size, (page - 1) * page_size + page_size
